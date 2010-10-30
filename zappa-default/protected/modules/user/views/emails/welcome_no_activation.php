d6b824e7de285f03b0e829349d65bdb2eff4986e
Karolis <karolis.n@gmail.com>
2010-10-30 13:49:35 +0100

    deleted zappa-default

--- zappa-default/protected/modules/user/views/emails/welcome_no_activation.php
+++
@@ -1,8 +0,0 @@
-<?php $this->layout = "application.views.layouts.email"; ?>
-<?php $login = $this->createAbsoluteUrl('/'); ?>
-<h2>You account at Chinchilla has been succesfully created!</h2>
-
-<p>You can login to Chinchilla anytime at <a href="<?php echo $login; ?>"><?php echo $login; ?></a>.</p>
-
-Kind regards,<br />
-Robot Chilla

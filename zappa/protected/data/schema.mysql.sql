d58e8536b420aad68b9f20f82f8cce2249208a20
Sebastian Borggrewe <sebastianborggrewe@c036004.wlan.net.ed.ac.uk>
2010-10-30 14:55:27 +0100

    changed phonenumber to string

--- zappa/protected/data/schema.mysql.sql
+++ zappa/protected/data/schema.mysql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 30, 2010 at 01:13 PM
+-- Generation Time: Oct 30, 2010 at 02:55 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.2
 
@@ -29,9 +29,6 @@ CREATE TABLE `tbl_calls` (
 -- Dumping data for table `tbl_calls`
 --
 
-INSERT INTO `tbl_calls` VALUES(1, 'Wake up bitch! It''s 7:30!');
-INSERT INTO `tbl_calls` VALUES(2, 'Wake up bitch! It''s 7:30!');
-INSERT INTO `tbl_calls` VALUES(3, 'Wake up bitch! It''s 7:30!');
 
 -- --------------------------------------------------------
 
@@ -42,17 +39,16 @@ INSERT INTO `tbl_calls` VALUES(3, 'Wake up bitch! It''s 7:30!');
 CREATE TABLE `tbl_job_queue` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `call_id` int(10) NOT NULL,
-  `phone` int(20) NOT NULL,
+  `phone` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `call_id_2` (`call_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `tbl_job_queue`
 --
 
-INSERT INTO `tbl_job_queue` VALUES(5, 3, 2147483647, '2010-10-30 13:10:55');
+INSERT INTO `tbl_job_queue` VALUES(5, 3, '2147483647', '2010-10-30 13:10:55');
 
 -- --------------------------------------------------------
 
